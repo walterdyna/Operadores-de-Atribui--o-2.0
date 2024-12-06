f2512d945a859937c4ad7f28a5a933b527a7cc6e
walterdyna <alexdyna@gmail.com>
2024-12-06 19:45:15 -0300

    first commit

---
+++ Operadores-de-Atribuição.js
@@ -0,0 +1,40 @@
+/*
+
+Operadores de Atribuição
+
+ = -> Atribuição
++= -> Adição
+-= -> Subtração
+/= -> Divisão
+*= -> Multiplicação
+%= -> Módulo (Resto da Divisão)
+
+*/
+
+const result = 20 // atibuição de valor ou texto
+
+result = result + 70 
+// e igual
+result += 70 // mesma coisa que a linha acima (codigo mais bonito)
+console.log(result) // imprime o valor de result que é 90
+
+result = result - 70
+// e igual
+result -= 70 // mesma coisa que a linha acima (codigo mais bonito)
+console.log(result) // imprime o valor de result que é 20
+
+result = result / 70
+// e igual
+result /= 70 // mesma coisa que a linha acima (codigo mais bonito)
+console.log(result) // imprime o valor de result que é 0.285714285714
+
+result = result * 70
+// e igual
+result *= 70 // mesma coisa que a linha acima (codigo mais bonito)
+console.log(result) // imprime o valor de result que é 20
+
+result = result % 70
+// e igual
+result %= 2 // mesma coisa que a linha acima (codigo mais bonito)
+console.log(result) // imprime o valor de result que é 0
+
